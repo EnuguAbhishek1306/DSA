aa5020bdcc0066c13ef8671115554c223357f009
Abhishek Reddy <91308697+EnuguAbhishek1306@users.noreply.github.com>
2024-07-05 05:21:21 +0000

    completed binary tree.py

--- BinaryTree.py
+++ BinaryTree.py
@@ -61,5 +61,102 @@ rightside.right=rightrchild
 # postOrder(root)
 # print("preorder")
 # preOrderTraversal(root)
-print("levelorder")
-levelOrder(root)
+# print("levelorder")
+# levelOrder(root)
+def search(rootNode,value):
+    if rootNode is None:
+        return "Not BT not exist"
+    else:
+        customQueue=queue.Queue()
+        customQueue.enqueue(rootNode)
+        while not customQueue.isempty() :
+            root=customQueue.dequeue()
+            if root.data == value:
+                return "success"
+            
+            if root.left:
+                customQueue.enqueue(root.left)
+            if root.right:
+                customQueue.enqueue(root.right)
+        return "Not Found"
+def insert(rootNode,newNode):
+    if rootNode is None:
+        rootNode=newNode
+    else:
+        cqueue=queue.Queue()
+        cqueue.enqueue(rootNode)
+        while not cqueue.isempty():
+            root=cqueue.dequeue()
+            if root.left:
+                cqueue.enqueue(root.left)
+            else:
+                root.left=newNode
+                return "successfully insereted"
+            if root.right:
+                cqueue.enqueue(root.right)
+            else:
+                root.right=newNode
+                return "successfully insereted"
+def getDeepestNode(rootNode):
+    if not rootNode:
+        return
+    else:
+        customqueue=queue.Queue()
+        customqueue.enqueue(rootNode)
+        while not customqueue.isempty():
+            root =customqueue.dequeue()
+            if root.left:
+                customqueue.enqueue(root.left)
+            if root.right:
+                customqueue.enqueue(root.right)
+        
+        return root
+def deletedeepNode(rootNode,DNode):
+    if not rootNode:
+        return
+    else:
+        customqueue=queue.Queue()
+        customqueue.enqueue(rootNode)
+        while not customqueue.isempty():
+            root = customqueue.dequeue()
+            if root==DNode:
+                root.data=None
+                return
+            if root.right:
+                if root.right is DNode:
+                    root.right=None
+                    return
+                else:
+                    customqueue.enqueue(root.right)
+            if root.left:
+                if root.left is DNode:
+                    root.left=None
+                    return
+                else:
+                    customqueue.enqueue(root.left)
+def deleteNode(rootNode,node):
+    if not rootNode:
+        return
+    else:
+        customqueue=queue.Queue()
+        customqueue.enqueue(rootNode)
+        while not customqueue.isempty():
+            root = customqueue.dequeue()
+            if root.data==node:
+                dNode=getDeepestNode(rootNode)
+                root.data=dNode.data
+                deletedeepNode(rootNode,dNode)
+                return "The node as been success fully deleted"
+            if root.left:
+                customqueue.enqueue(root.left)
+            if root.right:
+                customqueue.enqueue(root.right)
+        return "Failed to delete"
+def delete(rootNode):
+        rootNode.data=None
+        rootNode.left=None
+        rootNode.right=None
+        return "deleted succesfully"
+
+postOrder(root)
+
